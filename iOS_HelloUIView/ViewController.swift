10a1d6f5874af7d7b30f0e11ac89c8657a452ef4
Richy <richy@webcomm.com.tw>
2023-07-17 13:49:19 +0800

    製作介面畫面

--- iOS_HelloUIView/ViewController.swift
+++ iOS_HelloUIView/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var midRect: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
