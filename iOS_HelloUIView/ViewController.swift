abab05131c9a3a846ac0d8c7141d6334dd7cde3b
Richy <richy@webcomm.com.tw>
2023-07-17 13:49:42 +0800

    設定 view 的屬性

--- iOS_HelloUIView/ViewController.swift
+++ iOS_HelloUIView/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 設定 view 的屬性
+        // set background color
+        midRect.backgroundColor = .green // UIColor.green
+        
+        // set transparency 0~1 // 透明度
+        midRect.alpha = 1.0
+        
+        // hide uiview, make it invisible
+        midRect.isHidden = false
+        
     }
 
 
